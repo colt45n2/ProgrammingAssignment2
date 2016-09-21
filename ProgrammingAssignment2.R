74fdc1ae7f06338e58e8111d0fa7ebb55c66aa3e
colt45n2 <colton.a.smith@gmail.com>
2016-09-21 16:15:38 -0500

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,3 +1,9 @@
+## Cache the matrix for inversion 
+## This function creates a matrix which can store a corresponding inverse
+## Used to cache potentially time-consumed computations
+
+# this makecacheMatrix will create a special matix which can cache the inverse
+
 makecacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -11,6 +17,12 @@ makecacheMatrix <- function(x = matrix()) {
              setinverse = setinverse,
              getinverse = getinverse)
 }
+
+# this cacheSolve will return the inverse of the matrix
+# the function will first check to see if the inverse has been computed already
+# if the inverse has been computed and the matrix has not been edited, 
+# then the cacheSolve function skip the computation
+# if the inverse has not been computed then the cache will retrieve the inverse
 
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
